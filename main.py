c45de5cc7ac87416b9f0f108850589e486363605
DazielNguyen <duynguyenvananh@gmail.com>
2025-10-13 00:10:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/") # dùng để khai báo route
 def index():
-    return {'data' : {'name' : 'Van Anh Duy', 'age' : 21}}
+    return {'data' : 'Block list'}
 
-@app.get("/about")
-def about():
-    return {'data' : {'about page' : 'This is about page'}}
+@app.get("/blog/unpublished")
+def unpublished():
+    # fetch unpublished blogs
+    return {'data' : 'all unpublished blogs'}
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {'data' : id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # fetch comments of blog with id = id
+    return {'data' : {'1','2'}}
